ad7cc5c018d2008b77cb6d01ea19b34eb5078f94
Necdet UYGUR <necdet.uygur@gmail.com>
2021-12-05 01:40:01 +0300

    b.f.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,25 +6,15 @@ import Ikd from "./Ikd";
 import Modal from "./Modal";
 import Header from "./Header";
 import { getGold } from "../actions";
-import moment from "moment";
-import "moment/locale/tr";
 
 const App = (props: any) => {
-  const [tarih, setTarih] = useState("");
   useEffect(() => {
     props.getGold();
-    setInterval(() => {
-      setTarih(
-        moment(new Date(), "DD-MM-YYYY HH:mm:ss")
-          .locale("tr")
-          .format("DD MMMM YYYY dddd HH:mm:ss")
-      );
-    }, 1e3);
     // eslint-disable-next-line
   }, []);
   return (
     <div className="container-fluid mt-3">
-      <Header tarih={tarih} />
+      <Header />
       <div className="row">
         <div className="col">
           <Altin {...props.gold} />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,25 @@
-export default ({ tarih }: { tarih: string }) => (
-  <div style={{ height: "33px" }}>
-    <header className="text-center bg-dark text-light fixed-top border-secondary border-bottom p-1">
-      {tarih}
-    </header>
-  </div>
-);
+import { useState, useEffect } from "react";
+import moment from "moment";
+import "moment/locale/tr";
+
+const momentNow = () =>
+  moment(new Date(), "DD-MM-YYYY HH:mm:ss")
+    .locale("tr")
+    .format("DD MMMM YYYY dddd HH:mm:ss");
+
+export default () => {
+  const [tarih, setTarih] = useState(momentNow());
+  useEffect(() => {
+    setInterval(() => {
+      setTarih(momentNow());
+    }, 1e3);
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <div style={{ height: "33px" }}>
+      <header className="text-center bg-dark text-light fixed-top border-secondary border-bottom p-1">
+        {tarih}
+      </header>
+    </div>
+  );
+};
