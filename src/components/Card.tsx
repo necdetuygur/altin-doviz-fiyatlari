d86d6f0b443bb0a083b0650ea1471ab61ea40842
Necdet UYGUR <necdet.uygur@gmail.com>
2021-12-04 17:34:25 +0300

    tasarım

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ export default ({
     <div className="card-header">
       <img width="33" src={process.env.PUBLIC_URL + icon} alt="" />
       &nbsp;
-      <span className="ms-3">{title}</span>
+      <span className="ms-2">{title}</span>
     </div>
     <div className="card-body p-1">{children}</div>
   </div>
