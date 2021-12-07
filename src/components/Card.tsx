563d7162bf07f6d34fa67773dd10bc85d675226b
Necdet UYGUR <necdet.uygur@gmail.com>
2021-12-07 15:34:14 +0300

    card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ export default ({
   <div className="card bg-dark text-white mb-2">
     <div className="card-header">
       {icon && <img width="33" src={process.env.PUBLIC_URL + icon} alt="" />}
-      <span className="ms-2">{title}</span>
+      <span className={icon && "ms-2"}>{title}</span>
     </div>
     <div className="card-body p-0">{children}</div>
   </div>
