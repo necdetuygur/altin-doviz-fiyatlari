191801203a11020a47748b4aab8c8d22b0218f83
Necdet UYGUR <necdet.uygur@gmail.com>
2021-12-24 23:50:30 +0300

    modal full screen

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,6 +14,7 @@ const Modal = (props: any) => {
       toggle={modalToggle}
       isOpen={props.modalOpen}
       className={props.isDark ? "bg-dark text-white" : "bg-light text-dark"}
+      fullscreen={true}
     >
       <ModalHeader
         toggle={modalToggle}
