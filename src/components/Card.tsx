a15ff4332ba378456f28d89bab30310052d5c4c6
Necdet UYGUR <necdet.uygur@gmail.com>
2021-12-25 00:21:54 +0300

    card header

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,11 +3,13 @@ export default ({
   children,
   icon,
   isDark,
+  header,
 }: {
   title: string;
   children: any;
   icon?: string;
   isDark?: boolean;
+  header?: any;
 }) => (
   <div
     className={
@@ -17,6 +19,7 @@ export default ({
     <div className={"card-header " + (isDark ? "bg-dark" : "bg-light")}>
       {icon && <img width="33" src={process.env.PUBLIC_URL + icon} alt="" />}
       <span className={icon && "ms-2"}>{title}</span>
+      <div className="float-end">{header}</div>
     </div>
     <div className={"card-body p-0 " + (isDark ? "bg-dark" : "bg-light")}>
       {children}
