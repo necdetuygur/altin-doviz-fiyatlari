f228f6474f779f231d68431930fffde91186d70b
Necdet UYGUR <necdet.uygur@gmail.com>
2022-02-19 16:12:06 +0300

    Sorumluluk Reddi Beyanı

---
+++ src/components/SorumlulukReddiBeyani.tsx
@@ -0,0 +1,59 @@
+function SorumlulukReddiBeyani() {
+  return (
+    <div className="p-3">
+      <p>
+        Tüm veriler ve bilgiler, yalnızca bilgilendirme amacıyla "olduğu gibi"
+        sağlanmış olup alım satım amaçlı veya finansal, yatırım, vergi, yasal,
+        muhasebe ya da diğer konularda tavsiye niteliğinde değildir. Lütfen her
+        türlü alım satım işleminden önce fiyatları doğrulamak için aracı
+        kurumunuza veya mali temsilcinize danışın. "Altın Döviz Fiyatları";
+        yatırım danışmanı, mali danışman ya da hisse senedi aracısı değildir.
+        Veri ve bilgilerin hiçbiri, bir menkul kıymetin ya da finansal ürünün
+        satın alınmasına, satılmasına veya tutulmasına yönelik "Altın Döviz
+        Fiyatları" tarafından yapılan bir yatırım tavsiyesi, teklif, öneri ya da
+        talep değildir. "Altın Döviz Fiyatları", herhangi bir yatırımın tavsiye
+        edilebilirliği veya uygunluğu konusunda hiçbir taahhütte bulunmaz.
+      </p>
+      <p>
+        Veri ve bilgilerin hiçbiri, genel ya da özel yatırım tavsiyesi değildir.
+        Bu tür veri ve bilgilerde atıfta bulunulan finansal ürünler ya da
+        işlemler, yatırım profilinize ve yatırım hedeflerinize veya
+        beklentilerinize uygun olmayabilir. Herhangi bir finansal ürün veya
+        faaliyetin ilgi alanlarınıza, yatırım hedeflerinize, yatırım görüşünüze
+        ve risk anlayışınıza göre size uygun olup olmadığını değerlendirmek
+        sizin sorumluluğunuzdadır. "Altın Döviz Fiyatları", bu kapsamda atıfta
+        bulunulan finansal ürünlerle ilgili herhangi bir işlemden veya
+        yatırımdan kaynaklanan zararlardan sorumlu tutulamaz. "Altın Döviz
+        Fiyatları", herhangi bir yatırım kararının yalnızca sağlanan veri ve
+        bilgilere dayanarak verilmesini önermez.
+      </p>
+      <p>
+        Veriler, finans borsaları ve diğer içerik sağlayıcıları tarafından
+        sağlanmakta olup finans borsalarının veya diğer veri sağlayıcılarının
+        belirttiği şekilde gecikmeli olabilir. "Altın Döviz Fiyatları" hiçbir
+        veriyi doğrulamaz ve bunu yapma yükümlülüğünü reddeder.
+      </p>
+      <p>
+        "Altın Döviz Fiyatları", veri veya içerik sağlayıcıları, finansal
+        borsalar ve bağlı kuruluşlarının her biri ile iş ortakları (A) herhangi
+        bir verinin doğru, yeterli veya eksiksiz olduğunu açık şekilde reddeder
+        ve (B) bu tür verilerdeki hatalardan, eksikliklerden veya diğer
+        kusurlardan, gecikmelerden, kesintilerden ya da bunlara dayanarak
+        yapılan hiçbir işlemden sorumlu tutulamaz. "Altın Döviz Fiyatları" veya
+        veri sağlayıcılarımız, burada sunulan verileri kullanmanız nedeniyle
+        oluşabilecek herhangi bir zarardan sorumlu değildir. Burada kullanıldığı
+        şekliyle "iş ortakları" deyimi, "Altın Döviz Fiyatları" ile bu taraflar
+        arasındaki aracılık, ortaklık veya ortak girişim ilişkisini ifade etmez.
+      </p>
+      <p>
+        Burada bulunan hiçbir veri veya bilgi ile ilgili olarak önceden yazılı
+        izin almadan kopyalama, değiştirme, yeniden biçimlendirme, indirme,
+        depolama, yeniden oluşturma, yeniden işleme, iletme ya da yeniden
+        dağıtma işlemi yapmayacağınızı ve bu veri veya bilgileri ticari bir
+        kurumda kullanmayacağınızı kabul edersiniz.
+      </p>
+    </div>
+  );
+}
+
+export default SorumlulukReddiBeyani;
