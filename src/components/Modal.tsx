56970b1d837c42b123085fcaea47a206405e10d7
Necdet UYGUR <necdet.uygur@gmail.com>
2022-02-19 16:12:19 +0300

    Modal fullscreen disable

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,7 +14,7 @@ const Modal = (props: any) => {
       toggle={modalToggle}
       isOpen={props.modalOpen}
       className={props.isDark ? "bg-dark text-white" : "bg-light text-dark"}
-      fullscreen="lg"
+      size="lg"
     >
       <ModalHeader
         toggle={modalToggle}
